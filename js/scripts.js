ae1ba24ed6be0375f8dd819e6554da33d6f305a3
Georgina Van Dort and Rachael Bermudes <georgina@>
2016-06-09 16:39:27 -0700

    create arabic to roman numeral converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
-$("form#number").submit(function(event){
-    event.preventDefault();
-    var userInput = $("#arabic").val();
-    var output = rnumerals(userInput);
-    $("p").text(output);
-  });
-});
+debugger;
+$(document).ready(function(){
+  $("form#number").submit(function(event){
+   event.preventDefault();
+    var userInput = parseInt($("#arabic").val());
+    // var output = rnumerals(userInput);
+    //var result = rnumerals($("#arabic").val());
+  $("#result").text(output);
 // front end logic
 
-var rnumerals = function(){
-var numbers = [1, 2, 3]
-// for (var i = 0; i < numbers.length; i++) {
-  if (userInput === "1"){
-    var result = "i";
-  } else if (userInput === "2") {
-    var result = "ii";
-  } else if (userInput === "3") {
-    var result = "iii";
-  }
+var result = []
+var number1 = ["I","II","III","IV","V","VI","VII","VIII","IX"]
+var number2 = ["X","XX","XXX","LXXI","L","LX","LXX","LXXX","XC"]
+
+var result = number1[userInput-1];
+
+
+var result2 = number2[userInput-1];
+
+
+var userInput = $("#arabic").val();
+var nums = [];
+for (var i = 0; i < userInput.length; i++) {
+    nums.push(parseInt(userInput.charAt(i)));
 }
+
+
+  var result10 = number2[userInput-1];
+  var result1 = number1[userInput-1];
